073c7299cc0ec739264abc49025b078a00ccf417
Marymuriga <marymuriga@gmail.com>
2022-06-03 18:32:24 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,46 @@
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a-b;
+}
+function multiply(a, b){  
+    return a * b;}
+
+function divide(a, b){
+    return a / b;
+}  
+function increment(n){
+    return n + 1;
+}
+function decrement(n){
+    return n-1;} 
+
+
+function makeInt(n){
+    let a = parseInt(n, 10);
+     return a;
+}
+console.log(makeInt(2.2222));
+
+function preserveDecimal(n){
+    let b = parseFloat(n);
+    return b;
+}
+console.log(preserveDecimal("True"));
+
+
+ 
+
+
+ 
+
+
+
+
+    
+    
+  
+
+
+
